09d62fc0d75df168a2d020d834de75a1c371f1aa
Luke Cotton <lukecotton@icloud.com>
2018-09-03 13:26:30 -0400

    Now supports CORS requests.

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ var calculate = require("./calculate_router.js");
 
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use("/calculate", calculate);
 
 app.get("/", function(req, res) {
